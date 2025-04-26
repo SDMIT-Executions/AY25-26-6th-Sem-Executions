7e96f6c591bf878feabadd2189d8b47e729b3dcb
Shakthi-Jayabal <shakthijayabal@gmail.com>
2025-04-26 11:07:01 +0530

    Add files via upload

---
+++ AY25-26-6th Sem - EEE 1/Third.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int marks[]={80,90,72};
+    printf("\n%d",marks);
+     printf("\n%d",&marks);
+     printf("\n%d",marks[0]);
+     printf("\n%d",&marks[0]);
+       printf("\n%d",marks[1]);
+     printf("\n%d",&marks[1]);
+       printf("\n%d",marks[2]);
+     printf("\n%d",&marks[2]);
+    
+    return 0;
+}
